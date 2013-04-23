8736904298b4cf205a9b6842e952ee9194287661
Leif Johansson <leifj@sunet.se>
2013-04-23 10:44:24 +0200

    prep 0.2.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(here, 'README.md')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.2.1'
+version = '0.2.2dev'
 
 install_requires = [
     'threadpool',
