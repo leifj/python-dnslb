d6a1c3248bafdb282c5a6e82d56a9ac09747db2e
Leif Johansson <leifj@sunet.se>
2017-06-30 18:08:42 +0200

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(here, 'README.md')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.2.7'
+version = '0.2.8'
 
 install_requires = [
     'threadpool',
