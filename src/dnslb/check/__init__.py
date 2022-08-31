c93018f8dae04a2b8ca2974252348d0c53c61402
Martin van Es <martin@mrvanes.com>
2022-08-31 17:00:42 +0200

    Fix cert SNI checking

--- src/dnslb/check/__init__.py
+++ src/dnslb/check/__init__.py
@@ -1,7 +1,21 @@
+import ssl
 from ..exceptions import MonitorException
 import logging
 
 __author__ = 'leifj'
+
+
+class SNISSLContext(ssl.SSLContext):
+  def __new__(cls, protocol, server_hostname, *args, **kwargs):
+    return super().__new__(cls, protocol, *args, *kwargs)
+
+  def __init__(self, protocol, server_hostname, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self._server_hostname = server_hostname
+
+  def wrap_socket(self, sock, *args, **kwargs):
+    kwargs['server_hostname'] = self._server_hostname
+    return super().wrap_socket(sock, *args, **kwargs)
 
 
 def _check_http(host, vhost=None, url=None, match=None, use_tls=False, port=None):
@@ -9,12 +23,14 @@ def _check_http(host, vhost=None, url=None, match=None, use_tls=False, port=None
         if port is None:
             port = 443
         logging.debug("HTTPS connection to %s" % host)
-        h = httplib.HTTPSConnection("{0}:{1}".format(host, port))
+        context = SNISSLContext(ssl.PROTOCOL_TLS_CLIENT, vhost)
+        context.load_default_certs()
+        h = httplib.HTTPSConnection(host, port, context=context)
     else:
         if port is None:
             port = 80
         logging.debug("HTTP connection to %s:%d" % (host, port))
-        h = httplib.HTTPConnection("{0}:{1}".format(host, port))
+        h = httplib.HTTPConnection(host, port)
     logging.debug("GET %s (vhost=%s)" % (url, vhost))
     h.request('GET', url, "", {'Host': vhost})
     resp = h.getresponse()
